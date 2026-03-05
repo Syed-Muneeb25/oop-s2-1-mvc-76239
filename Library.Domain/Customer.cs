f75f6fb6ed5282298187374951de62dbcc623593
Syed Muneeb <muneebmurshid786@gmail.com>
2026-03-05 18:42:19 +0000

    Setup

---
+++ Library.Domain/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Domain
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+
+        public List<Invoice> Invoices { get; set; } = new();
+    }
+}
