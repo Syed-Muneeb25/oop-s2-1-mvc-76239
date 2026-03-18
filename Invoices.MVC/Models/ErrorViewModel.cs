e82c19fd51a649afc1d9a28fe845784304a82eb5
Syed Muneeb <muneebmurshid786@gmail.com>
2026-03-18 03:36:23 +0000

    removed Invoice.MVC project

--- Invoices.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Invoices.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
